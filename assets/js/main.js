1d37ebc4b6024f30d6b91cc180547f94a413eae5
avishkaindunil <118374198+avishkaindunil@users.noreply.github.com>
2023-06-06 23:11:40 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,8 +64,8 @@
     let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000*60*60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000*60));
     let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
     countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
